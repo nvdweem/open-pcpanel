07c9c5616621e7c2c5e55da5db7143cdc91e9a43
Niels van de Weem <n.van.de.weem@smile.nl>
2021-05-29 10:06:59 +0200

    Adding front-end code and build step

--- core/src/main/java/dev/niels/pcpanel/volume/VolumeControl.java
+++ core/src/main/java/dev/niels/pcpanel/volume/VolumeControl.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 @Slf4j
 public class VolumeControl {
-    public static void main(String... args) {
+    public static void xmain(String... args) {
         System.out.println("Test!");
 
         var dIds = new ArrayList<String>();
