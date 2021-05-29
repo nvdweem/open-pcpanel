8a557d4ee586655b5290344328d773490a80a212
Niels van de Weem <n.van.de.weem@smile.nl>
2021-05-29 10:48:17 +0200

    Fix build and angular compatibility

--- core/src/main/java/dev/niels/pcpanel/PcPanelApplication.java
+++ core/src/main/java/dev/niels/pcpanel/PcPanelApplication.java
@@ -1,16 +1,13 @@
 package dev.niels.pcpanel;
 
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
 public class PcPanelApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(PcPanelApplication.class)
-                .web(WebApplicationType.NONE)
-                .run(args);
+        SpringApplication.run(PcPanelApplication.class, args);
     }
 }
