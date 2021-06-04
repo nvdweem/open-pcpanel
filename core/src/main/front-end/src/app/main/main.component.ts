125f394b7a1ae0b8e3645e714795bdae948d72cf
Niels van de Weem <n.van.de.weem@smile.nl>
2021-06-04 20:57:16 +0200

    Allow theme toggle

--- core/src/main/front-end/src/app/main/main.component.ts
+++ core/src/main/front-end/src/app/main/main.component.ts
@@ -10,6 +10,8 @@ import {map, shareReplay} from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent {
+  private _darkMode = true;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
@@ -17,6 +19,24 @@ export class MainComponent {
     );
 
   constructor(private breakpointObserver: BreakpointObserver) {
+    this.darkMode = localStorage.getItem('darkmode') !== 'false';
   }
 
+  toggleDarkMode(): void {
+    this.darkMode = !this.darkMode;
+  }
+
+  set darkMode(dm: boolean) {
+    if (!dm) {
+      document.body.classList.add('light');
+    } else {
+      document.body.classList.remove('light');
+    }
+    localStorage.setItem('darkmode', String(dm));
+    this._darkMode = dm;
+  }
+
+  get darkMode(): boolean {
+    return this._darkMode;
+  }
 }
