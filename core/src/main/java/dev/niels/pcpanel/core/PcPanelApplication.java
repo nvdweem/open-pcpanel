e809a5cf45558e6cec272181e2fc5bd5cda08366
Niels van de Weem <n.van.de.weem@smile.nl>
2021-06-11 19:44:40 +0200

    Always (de)serialize colors using the Color(De)Serializers

--- core/src/main/java/dev/niels/pcpanel/core/PcPanelApplication.java
+++ core/src/main/java/dev/niels/pcpanel/core/PcPanelApplication.java
@@ -1,8 +1,12 @@
 package dev.niels.pcpanel.core;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.awt.Color;
 
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = {"dev.niels", "pcpanel"})
@@ -11,5 +15,12 @@ public class PcPanelApplication {
     new SpringApplicationBuilder(PcPanelApplication.class)
       .headless(false)
       .run(args);
+  }
+
+  @Bean
+  public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+    return builder -> builder
+      .serializerByType(Color.class, new JsonColor.ColorSerializer())
+      .deserializerByType(Color.class, new JsonColor.ColorDeserializer());
   }
 }
